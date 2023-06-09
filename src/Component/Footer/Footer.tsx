9f21aa8cba051246b5f840ac6aac696f072fd791
saakeeb <sakib14april@gmail.com>
2023-06-10 00:13:39 +0600

    Feature: footer

--- src/Component/Footer/Footer.tsx
+++ src/Component/Footer/Footer.tsx
@@ -30,20 +30,23 @@ const Footer = () => {
       <div className="relative z-40 ">
         <div className="animation">
           <svg
-            className="waves w-full h-24 sm:h-40"
+            className="waves"
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
             viewBox="0 24 150 28"
             preserveAspectRatio="none"
-            shapeRendering="auto"
+            shape-rendering="auto"
           >
             <defs>
-              <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+              <path
+                id="gentle-wave"
+                d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+              ></path>
             </defs>
             <g className="parallax">
-              <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgb(244,161,38,0.8)" ref={waveRef1} />
-              <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgb(244,161,38,0.5)" ref={waveRef2} />
-              <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgb(244,161,38,0.1)" ref={waveRef3} />
+              <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgb(244 161 38 / 80%)" ref={waveRef1} />
+              <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgb(244 161 38 / 50%)" ref={waveRef2} />
+              <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgb(244 161 38 / 30%)" ref={waveRef3} />
               <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" ref={waveRef4} />
             </g>
           </svg>
