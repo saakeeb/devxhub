94518a92df5af828c266048f59cb79a34b05759a
saakeeb <sakib14april@gmail.com>
2023-06-12 18:43:49 +0600

    Feature: video bg

--- src/Component/VideoPlayer/MobileVideoPlayer.tsx
+++ src/Component/VideoPlayer/MobileVideoPlayer.tsx
@@ -1,17 +1,11 @@
 import React, { useRef, useLayoutEffect, useEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import bannerVideo from '../../assets/Videos/Animation_Video_Desktop_Device.mp4';
-import MorphingTextComponent from '../MorphingText/MorphingTextComponent';
-import GrowthCommunityComponent from '../GrowthCommunity/GrowthCommunityComponent';
-import MobileVideoPlayer from './MobileVideoPlayer';
-// import IphoneBG from '../../assets/Webp Images/iPhone.webp';
-// import monstera from '../../assets/Webp Images/monstera-small.webp';
-// import mobBG from '../../assets/Webp Images/mobBG.webp';
+import ipadVideo from '../../assets/Videos/ipad.mp4';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const VideoPlayer: React.FC = () => {
+const MobileVideoPlayer: React.FC = () => {
   // make references
   const videoRef = useRef<HTMLVideoElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
@@ -104,39 +98,14 @@ const VideoPlayer: React.FC = () => {
   }, []);
 
   return (
-    <div className="relative">
-      <section className="relative overflow-hidden">
-        <video // video for desktop view in the bg
-          ref={videoRef}
-          src={bannerVideo}
-          muted
-          loop
-          className="object-fill fixed h-screen w-full z-10 top-0 right-0 bottom-0 left-0 hidden md:block"
-        />
-        <MobileVideoPlayer />
-        {/* <img // image for mobile view in the bg
-          src={mobBG}
-          alt="Mobile bottom Background"
-          className="md:hidden block z-10 object-fill fixed h-screen top-0 right-0 bottom-0 left-0 w-screen"
-        /> */}
-        {/* <img
-          src={IphoneBG}
-          alt=" Hero Top Background"
-          className="md:hidden block z-20 absolute top-0 right-0 bottom-0 left-0 w-screen"
-        /> */}
-        {/* <img
-          src={monstera}
-          alt=" Iphone Background with leaf"
-          className="md:hidden block z-30 absolute top-20 left-0 w-36"
-        /> */}
-
-        <MorphingTextComponent />
-        <div className="lg:h-screen lg:block hidden"></div>
-        <GrowthCommunityComponent />
-        <div className="lg:h-screen lg:block hidden"></div>
-      </section>
-    </div>
+    <video // video for mobile view in the bg
+      ref={videoRef}
+      src={ipadVideo}
+      muted
+      loop
+      className="object-fill fixed h-screen w-full z-10 top-0 right-0 bottom-0 left-0 block md:hidden"
+    />
   );
 };
 
-export default VideoPlayer;
+export default MobileVideoPlayer;
