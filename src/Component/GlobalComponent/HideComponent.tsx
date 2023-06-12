ae4eb69e170c4e75ef0efd652a8fdfad08512ee2
saakeeb <sakib14april@gmail.com>
2023-06-12 19:24:02 +0600

    Feature: component hidden

--- src/Component/GlobalComponent/HideComponent.tsx
+++ src/Component/GlobalComponent/HideComponent.tsx
@@ -1,16 +1,31 @@
 import React, { ComponentType, useEffect } from 'react';
-import hideReactDevtools from 'react-devtools-inline';
 
 const HideComponents = <Props extends object>(Component: ComponentType<Props>) => {
   const HiddenComponent: React.FC<Props> = (props) => {
     useEffect(() => {
-      hideReactDevtools; // Hide the React element tree in devtools
+      const devToolsStyle = `
+        visibility: hidden !important;
+        pointer-events: none !important;
+        display: none !important;
+      `;
+      const devToolsMetaTag = document.createElement('meta');
+      devToolsMetaTag.name = 'react-devtools';
+      devToolsMetaTag.content = 'no-display';
+      document.head.appendChild(devToolsMetaTag);
+      const devToolsRoot = document.createElement('div');
+      devToolsRoot.style.cssText = devToolsStyle;
+      document.body.appendChild(devToolsRoot);
+
+      return () => {
+        document.head.removeChild(devToolsMetaTag);
+        document.body.removeChild(devToolsRoot);
+      };
     }, []);
 
     return <Component {...props} />;
   };
 
-  HiddenComponent.displayName = 'Dev'; // Give the component name "Dev" in chrome dev tools in components tab
+  HiddenComponent.displayName = 'Dev'; // Give the component name "Dev" in Chrome DevTools in the components tab
 
   return HiddenComponent;
 };
